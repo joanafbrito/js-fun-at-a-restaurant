82d87bc2bec0787c6424eabce36520360e6b98c9
Joana Brito <joanafiorentini@hotmail.com>
2021-05-15 15:34:47 -0700

    Add work on orders

--- src/meal.js
+++ src/meal.js
@@ -8,14 +8,13 @@ function createMenuItem(name, price, type) {
     price: price,
     type: type,
   }
+
   return menuItem
 };
 
 function addIngredients(ingredient1, ingredients) {
-
-if (ingredients.includes(ingredient1)) {
-  return
-
+  if (ingredients.includes(ingredient1)) {
+    return
   } else {
     return ingredients.push(ingredient1)
   }
@@ -25,21 +24,23 @@ if (ingredients.includes(ingredient1)) {
 
 function formatPrice(initialPrice) {
   return `$${initialPrice}`
-
 };
 
 function decreasePrice(price) {
-  var discount = (price * .10)
-  var decreasePrice = (price - discount)
+  var decreasePrice = (price * .90)
+  // var discount = (price * .10)
+  // var decreasePrice = (price - discount)
   return decreasePrice
 };
 
 function createRecipe(title,ingredients,menuItemType) {
-  return recipe = {
+  var recipe = {
     title: title,
     ingredients: ingredients,
     type: menuItemType,
   }
+  
+  return recipe
 };
 
 
--- src/order.js
+++ src/order.js
@@ -1,7 +1,54 @@
+function takeOrder(newOrder, deliveryOrders) {
+  // if deliveryOrders < 3 ADD new order
+  //
+  if (deliveryOrders.length < 3) {
+    deliveryOrders.push(newOrder);
+  }
+
+  return deliveryOrders;
+
+}
+
+function refundOrder(orderNumber, deliveryOrders) {
+  // if deliveryOrders > 2 remove order by number pay attention on the order number
+  // if (deliveryOrders.length > 2) {
+  // is not asking for
+    // deliveryOrders.shift()- not the best one here 
+  // this is for the first test
+  for (var i = 0; i < deliveryOrders.length; i++ ) {
+    if(deliveryOrders[i].orderNumber === orderNumber) {
+    deliveryOrders.splice(i, 1)
+  }
+}
+  return deliveryOrders;
+}
+
+function listItems(deliveryOrders) {
+    // list the order itens by name // dif order itens by name //
+ var orderNames = [];
+for (var i = 0; i < deliveryOrders.length; i++ ) {
+    orderNames.push(deliveryOrders[i].item)
+}
+return orderNames.join(", ");
+
+}
+
+function searchOrder(deliveryOrders, food) {
+  var orderNames = [];
+ for (var i = 0; i < deliveryOrders.length; i++ ) {
+     orderNames.push(deliveryOrders[i].item)
+ }
+
+ return orderNames.includes(food);
+
+}
+
+
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
